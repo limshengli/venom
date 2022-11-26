e965fb3c8fd14a0a945877ea438e392c60071fae
limshengli <limshengli@sql.com.my>
2022-11-26 13:04:55 +0800

    Allow checkNumberStatus to verify WhatsApp group ID

--- src/lib/wapi/functions/check-number-status.js
+++ src/lib/wapi/functions/check-number-status.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-undef */
 export async function checkNumberStatus(id, conn = false) {
   try {
-    const err = { error: 404 };
+    const err = {
+      error: 404
+    };
+
     const connection =
       window.Store &&
       window.Store.State &&
@@ -8,14 +12,6 @@ export async function checkNumberStatus(id, conn = false) {
       window.Store.State.Socket.state
         ? window.Store.State.Socket.state
         : '';
-    const checkType = WAPI.sendCheckType(id);
-    if (!!checkType && checkType.status === 404) {
-      Object.assign(err, {
-        text: checkType.text,
-        numberExists: null
-      });
-      throw err;
-    }
 
     if (conn === true) {
       if (connection !== 'CONNECTED') {
@@ -27,13 +23,42 @@ export async function checkNumberStatus(id, conn = false) {
         throw err;
       }
     }
-
     const lid = await WAPI.getChat(id);
     if (lid) {
-      return await Store.checkNumber
-        .queryWidExists(lid.id)
+      if (id.endsWith('@g.us')) {
+        return await WPP.group
+          .ensureGroup(lid.id)
+          .then((result) => {
+            if (!!result && typeof result === 'object') {
+              const data = {
+                status: 200,
+                numberExists: true,
+                id: result.id,
+              };
+              return data;
+            }
+            throw Object.assign(err, {
+              connection: connection,
+              numberExists: false,
+              text: `The number does not exist`
+            });
+          })
+          .catch((err) => {
+            if (err.text) {
+              throw err;
+            }
+            throw Object.assign(err, {
+              connection: connection,
+              numberExists: false,
+              text: err
+            });
+          });;
+      }
+
+      return await WPP.contact
+        .queryExists(lid.id)
         .then((result) => {
-          if (!!result && typeof result === 'object') {
+          if (typeof result === 'object') {
             const data = {
               status: 200,
               numberExists: true,
